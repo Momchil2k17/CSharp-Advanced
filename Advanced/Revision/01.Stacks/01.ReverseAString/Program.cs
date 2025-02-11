3942b60327e79a5e18946613dab59a9c6c3e62db
Momchil Yankov <momchil200067@gmail.com>
2025-02-11 14:50:38 +0200

    Started general revision on funds in c#

---
+++ Advanced/Revision/01.Stacks/01.ReverseAString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>(input.Length);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            StringBuilder sb = new StringBuilder(input.Length);
+            while (stack.Count > 0)
+            {
+                sb.Append(stack.Pop());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ Advanced/Revision/01.Stacks/02.StackSum/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> ints = new Stack<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x)));
+            string com = Console.ReadLine().ToLower();
+            while (com != "end")
+            {
+                string[] tokens = com.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] == "add")
+                {
+                    ints.Push(int.Parse(tokens[1]));
+                    ints.Push(int.Parse(tokens[2]));
+                }
+                else if (tokens[0] == "remove")
+                {
+                    if (int.Parse(tokens[1]) <= ints.Count)
+                    {
+                        for (int i = 0; i < int.Parse(tokens[1]); i++)
+                        {
+                            ints.Pop();
+                        }
+                    }
+                }
+                com = Console.ReadLine().ToLower();
+            }
+            
+            Console.WriteLine($"Sum: {ints.Sum()}");
+        }
+    }
+}
